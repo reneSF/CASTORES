008b235f15a58fb52b8ae2a49d49fbf691f45436
r.lopezmedina@ugto.mx <92562562+reneSF@users.noreply.github.com>
2026-02-10 22:18:35 -0600

    Ajuste a comunicación de puertos y test de los controlers listo

--- inventario/src/main/java/com/castores/inventario/controller/AuthController.java
+++ inventario/src/main/java/com/castores/inventario/controller/AuthController.java
@@ -1,30 +1,15 @@
 package com.castores.inventario.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/auth")
 public class AuthController {
 
-    @GetMapping("/")
+    @GetMapping("/login")
     public String login() {
-        return "login";
-    }
-
-    @PostMapping("/login")
-    public String procesarLogin(@RequestParam String correo,
-                                @RequestParam String password,
-                                Model model) {
-
-        // TEMPORAL (luego conectamos BD)
-        if (correo.equals("admin@castores.com") && password.equals("1234")) {
-            return "redirect:/inventario";
-        }
-
-        model.addAttribute("error", "Credenciales incorrectas");
         return "login";
     }
 }
